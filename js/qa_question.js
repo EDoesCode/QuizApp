df1f4a719c12691a375a2a6dc4d0553251980e89
michaelasantiago <michael.alec.santiago@gmail.com>
2019-07-24 21:04:39 -0400

    Question create/modify form added

--- js/qa_question.js
+++ js/qa_question.js
@@ -10,26 +10,51 @@ questions: object[]: array of Question objects;
 */
 function loadTable(questions)
 {
+    // Getting correct answer from answers
+    for (var i = 0; i < questions.length; i++)
+    {
+        let curQuestion = questions[i];
+        let answerLetter = curQuestion.answer.toLowerCase();
+        curQuestion.fullAnswer = curQuestion[answerLetter];
+    }
     let headers = ["Question", "Answer"];
-    let keys = ["question", "answer"];
-    let table = makeTable(TABLE_CRUD, headers, keys, questions, modifyQuestion, deleteQuestion);
+    let keys = ["question", "fullAnswer"];
+    let table = makeTable(TABLE_CRUD, headers, keys, questions);
     table.attr("id", "questionTable");
     $(tableDiv).append(table);
     buildSearchAddBar();
 }
 
-/* Loads the div for modifying the given question by id
-id: int: Database ID for the given question
+/* Populates the fields of the data entry form when modifying an existing entry.  Each page needs this function
+curData: Question: data object to populate the fields with
 */
-function modifyQuestion(id)
+function populateFields(curData)
 {
-    window.alert("modify" + id);
+    if (curData === null)
+        return;
+    $(question).val(curData.question);
+    $(answerA).val(curData.a);
+    $(answerB).val(curData.b);
+    $(answerC).val(curData.c);
+    $(answerD).val(curData.d);
+    $(answerE).val(curData.e);
+    $("input[name=answer][value=" + curData.answer+"]").prop('checked', true);
 }
 
-/* Deletes the selected question from the database
-id: int: Database ID for the given question
-*/
-function deleteQuestion(id)
+function getDataObject()
 {
-    window.alert("delete" + id);
+    var dataObj = {};
+    dataObj.question = $(question).val;
+    if (dataObj.question === "")
+        throw "Question must have a question body.";
+    dataObj.a = $(answerA).val();
+    dataObj.b = $(answerB).val();
+    dataObj.c = $(answerC).val();
+    dataObj.d = $(answerD).val();
+    dataObj.e = $(answerE).val();
+    dataObj.answer =  $("input[name=answer]:checked").val();
+    if (!dataObj[dataObj.answer])
+        throw "Selected answer cannot be blank.";
+    console.log(dataObj);
+    return dataObj;
 }
