9d8dcb6ae6c23db1c39175d4c784701e636c4758
michaelasantiago <michael.alec.santiago@gmail.com>
2019-07-25 14:49:39 -0400

    Changing baseURL to fullernetwork

--- js/baseURL.js
+++ js/baseURL.js
@@ -1,6 +1,6 @@
 // localStorage.baseURL = "file:///D:/UCF/COP4331/LargeProject/";
 // globalVariable={baseURL:"file:///D:/UCF/COP4331/LargeProject/"}; 
-const baseURL = "file:///D:/UCF/COP4331/LargeProject/"
+const baseURL = "https://fullernetwork.com/";
 
 // Indicates whether to call the server for data or to use hardcoded test data
 const onServer = true;
